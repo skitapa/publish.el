341216b4ee348fa9f91c595318849d64e1056965
Jimmy Arogén <jimmy@Jimmys-MBP.opennet.oxelosund.bahnhof.se>
2015-03-06 10:47:02 +0100

    Added support for mounting within GNU Emacs

--- publish.el
+++ publish.el
@@ -1,14 +1,13 @@
 ;;; publish.el --- Copy a buffer to a sshfs mounted folder somewhere
 ;; Copyright (C) 2011 Free Software Foundation, Inc.
 ;; Author Jimmy Arogén
-;; version 0.000001
+;; version 0.11
 ;; keywords: publish, sshfs
 ;; URL: https://github.com/skitapa/publish.el
+;;; Code:
 ;;; Commentary:
 ;; This package helps copy a buffer to a designated place somewhere. This can be used
 ;; to publish files if that folder is mounted with sshfs.
-;;; Code:
-
 
 (defun publish ()
   "Save file to remotely mounted directory for publishing.
@@ -24,9 +23,15 @@ that directory"
 	  (write-region nil (point-max)
 			(concat publish_dir (substring (buffer-file-name) 29))))
 	(deactivate-mark)
-	(goto-char save-point)
-	)
-    (message "The directory is not mounted!")))
+	(goto-char save_point))
+    (progn
+      (if (y-or-n-p "Server directory not mounted.  Mount it? ")
+	  (progn
+	    (start-process "pub_mount" (get-buffer-create "*pub_mount-buffer*")
+			   "/Users/johndoe/bin/pub_mount.sh")
+	    (message "Directory mounted, publish again for it to take effect"))
+	(progn
+	  (message "Not mounting directory, publication failed"))))))
 
 (provide 'publish)
 ;;; publish.el ends here
